42ea916b7dbcccb88c10334ec1688f1b2a0d7b4b
GromVlad <gromvlad12@gmail.com>
2020-10-22 13:15:01 +0300

    add getMax and getMin

--- Index.js
+++ Index.js
@@ -1,1 +1,56 @@
-// @TODO: Code here
+function getMax(n) {
+    const firstDigit = n % 10;
+    const remainN = Math.trunc(n / 10);
+
+    const recurDeep = 0
+
+    return getMaxStep(remainN, firstDigit, recurDeep);
+}
+
+function getMaxStep(remainNumbers, currentMaxNumber, recurDeep) {
+
+    if (remainNumbers <= 1) {
+        return {
+            'MaxNumber': currentMaxNumber,
+            'RecurDeep': recurDeep,
+        };
+    }
+
+    const digit = remainNumbers % 10;
+    const remainN = Math.trunc(remainNumbers / 10);
+
+    if (currentMaxNumber < digit) {
+        return getMaxStep(remainN, digit, recurDeep + 1);
+    } else {
+        return getMaxStep(remainN, currentMaxNumber, recurDeep + 1);
+    }
+
+}
+
+function getMin(n) {
+    const firstDigit = n % 10;
+    const remainN = Math.trunc(n / 10);
+
+    return getMinStep(remainN, firstDigit, 0);
+}
+
+function getMinStep(remainNumbers, currentMinNumber, recurDeep) {
+
+    if (remainNumbers <= 1) {
+        return {
+            'MinNumber': currentMinNumber,
+            'RecurDeep': recurDeep,
+        };;
+    }
+
+    const digit = remainNumbers % 10;
+    const remainN = Math.trunc(remainNumbers / 10);
+
+
+    if (currentMinNumber > digit) {
+        return getMinStep(remainN, digit, recurDeep + 1);
+    } else {
+        return getMinStep(remainN, currentMinNumber, recurDeep + 1);
+    }
+
+}
